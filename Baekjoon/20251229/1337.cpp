4693fc16463e6b436ae69959eaa63f3392cf208c
hyjnkkr <therealguy03@gmail.com>
2026-01-04 18:33:11 +0900

    1337

---
+++ Baekjoon/20251229/1337.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+vector<int> v;
+vector<int> sub;
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+
+	int n;
+	cin >> n;
+	
+	for(int i = 0; i < n; i++) {
+		int tmp;
+		cin >> tmp;
+		v.push_back(tmp);
+	}
+
+	sort(v.begin(), v.end());
+
+	int ans = 0;
+	for(int i = 0; i < v.size(); i++) {
+		int cnt = 0;
+		for(int j = 1; j < 5 && i + j < v.size(); j++) {
+			if(v[i] + 5 > v[i + j]) cnt++;
+		}
+		ans = max(ans, cnt);
+	}
+
+	cout << 4 - ans;
+
+	return 0;
+}
